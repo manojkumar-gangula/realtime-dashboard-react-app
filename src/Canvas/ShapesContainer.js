c55dd40cddc16ec8ffb172dcb34c415734dfcbe8
Manoj Kumar Gangula <mkgangula1@gmail.com>
2025-06-09 23:34:08 +0530

    hover shape icon for shapes

--- src/Canvas/ShapesContainer.js
+++ src/Canvas/ShapesContainer.js
@@ -7,16 +7,31 @@ import {
   Triangle,
   Minus,
   Pentagon,
+  Shapes,
 } from "lucide-react";
+import "./ShapesContainer.css";
 
 function ShapesContainer({ addShape }) {
   return (
     <div className="icons-container">
-      <Square />
-      <Circle />
-      <Triangle />
-      <Minus />
-      <Pentagon />
+      <Shapes className="icon" />
+      <div className="icons">
+        <div className="my-hover-effect icon">
+          <Square />
+        </div>
+        <div className="my-hover-effect icon">
+          <Circle />
+        </div>
+        <div className="my-hover-effect icon">
+          <Triangle />
+        </div>
+        <div className="my-hover-effect icon">
+          <Pentagon />
+        </div>
+        <div className="my-hover-effect icon">
+          <Minus />
+        </div>
+      </div>
     </div>
   );
 }
